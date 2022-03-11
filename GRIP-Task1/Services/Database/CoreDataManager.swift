0f12b3ee24b34b86e3c2f31f8151451c7787ca8e
Noor El-Din Walid <noorwalid72@yahoo.com>
2022-03-11 23:08:32 +0200

    Added Constants file and created the Core Data Stack.

---
+++ GRIP-Task1/Services/Database/CoreDataManager.swift
@@ -0,0 +1,61 @@
+//
+//  CoreDataManager.swift
+//  GRIP-Task1
+//
+//  Created by Noor Walid on 11/03/2022.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataManager {
+    private let modelName: String
+    
+    private lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
+        let persistentStoreCoordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
+
+        let fileManager = FileManager.default
+        let storeName = "\(self.modelName).sqlite"
+
+        let documentsDirectoryURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0]
+
+        let persistentStoreURL = documentsDirectoryURL.appendingPathComponent(storeName)
+
+        do {
+            try persistentStoreCoordinator.addPersistentStore(ofType: NSSQLiteStoreType,
+                                                              configurationName: nil,
+                                                              at: persistentStoreURL,
+                                                              options: nil)
+        } catch {
+            fatalError("Unable to Load Persistent Store")
+        }
+
+        return persistentStoreCoordinator
+    }()
+    
+    private(set) lazy var managedObjectContext: NSManagedObjectContext = {
+        let managedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+
+        managedObjectContext.persistentStoreCoordinator = self.persistentStoreCoordinator
+
+        return managedObjectContext
+    }()
+    
+    private lazy var managedObjectModel: NSManagedObjectModel = {
+        guard let modelURL = Bundle.main.url(forResource: self.modelName, withExtension: "momd") else {
+            fatalError("Unable to Find Data Model")
+        }
+
+        guard let managedObjectModel = NSManagedObjectModel(contentsOf: modelURL) else {
+            fatalError("Unable to Load Data Model")
+        }
+
+        return managedObjectModel
+    }()
+    
+    init(modelName: String) {
+        self.modelName = modelName
+    }
+    
+    
+}
---
+++ GRIP-Task1/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GRIP-Task1
+//
+//  Created by Noor Walid on 11/03/2022.
+//
+
+import Foundation
+
+struct Constants {
+    static let coreDataModelName = "Model"
+}
